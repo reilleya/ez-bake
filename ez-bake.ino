483a6453b6c0e02e3bc62f83de7fa3fb431d5384
Andrew Reilley <andrew.r.reilley@gmail.com>
2023-01-07 21:55:18 -0500

    Add basic example code

--- ez-bake.ino
+++ ez-bake.ino
@@ -1,7 +1,27 @@
-void setup() {
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "constants.h"
 
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+void setup(void) {
+  Serial.begin(SERIAL_BAUDRATE);
+  sensors.begin();
 }
 
-void loop() {
+void loop(void) {
+  sensors.requestTemperatures();
+  for (int i = 0; i < NUM_THERMOCOUPLES; i++ ) {
+    float tempC = sensors.getTempCByIndex(i);
 
+    if (tempC != DEVICE_DISCONNECTED_C) {
+      Serial.print(tempC);
+      Serial.print(" ");
+    } else {
+      Serial.print("X ");
+    }
+  }
+  Serial.println();
 }
