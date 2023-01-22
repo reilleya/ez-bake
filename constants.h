7e16fa2855ec492f9e1b1c62122a33a297c178db
Sam Austin <austinsp@mit.edu>
2023-01-21 22:11:16 -0800

    Added support for fan PWM control and tach feedback

--- constants.h
+++ constants.h
@@ -7,4 +7,15 @@
 #define MIN_LEGAL_TEMP_C -20
 #define MAX_LEGAL_TEMP_C 150
 
+// Fan control and feedback
+#define FAN_1_CTRL_PIN 4
+#define FAN_2_CTRL_PIN 5
+#define FAN_1_TACH_PIN 6
+#define FAN_2_TACH_PIN 7
+#define TACH_PULSE_MICROS_MIN 1000
+#define HZ2RPM 60
+#define MICROS2SEC 1000000.
+
+#define FAN_PWM_FREQ 25000
+
 typedef float tempCArray[NUM_THERMOCOUPLES];
